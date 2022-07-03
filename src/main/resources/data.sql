0d1da2013af60722e3be1b37bffb5997d06347a3
Dmitry <dschinghis2008@yandex.ru>
2022-07-03 12:15:52 +0700

    fix: исправить замечания

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -20,14 +20,3 @@ insert into GENRES (NAME)
 values ('Документальный');
 insert into GENRES (NAME)
 values ('Боевик');
-/*
-insert into FILMS (NAME, DESCRIPTION, RELEASEDATE, DURATION, RATE, MPA)
-values ('film1','desc film2','1990-01-01',60,0,1);
-insert into FILMS (NAME, DESCRIPTION, RELEASEDATE, DURATION, RATE, MPA)
-values ('film2','desc film2','2000-01-01',60,0,1);
-insert into FILM_GENRE_LINK (ID_GENRE, ID_FILM)
-values (1,1);
-insert into FILM_GENRE_LINK (ID_GENRE, ID_FILM)
-values (3,2);
-insert into RATING (id_user,id_film)
-values (1,1)*/
